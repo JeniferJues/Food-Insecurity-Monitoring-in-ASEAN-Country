a1495d68f4485ac414adbac65250242ca76e5eaf
Jenifer Mayang Jues <139893426+JeniferJues@users.noreply.github.com>
2026-02-24 22:51:26 +0800

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,20 +1,15 @@
 import pandas as pd
+import joblib
 
-def preprocess_data(df):
+def preprocess_input(user_input_dict):
 
-    X = df.drop(["Food Insecurity Rate","Area","Year"], axis=1)
-    y = df["Food Insecurity Rate"]
+    # Load correct feature order
+    feature_columns = joblib.load("models/prediction/feature_columns.pkl")
 
-    X = pd.get_dummies(X, drop_first=True)
+    # Convert user input to DataFrame
+    input_df = pd.DataFrame([user_input_dict])
 
-    return X, y
+    # Ensure correct order
+    input_df = input_df[feature_columns]
 
-
-def preprocess_input(irrigation, water):
-
-    data = pd.DataFrame({
-        "irrigation":[irrigation],
-        "water_access":[water]
-    })
-
-    return data
+    return input_df
