ec1452256fe242d72c03e4168043262281bbb8cd
Jenifer Mayang Jues <139893426+JeniferJues@users.noreply.github.com>
2026-02-24 19:04:07 +0800

    Add files via upload

---
+++ scripts/train_forecast_models.py
@@ -0,0 +1,26 @@
+(Multi-country Prophet models)
+import pandas as pd
+import joblib
+from prophet import Prophet
+
+df = pd.read_csv("data/model_df.csv")
+
+countries = df["Area"].unique()
+
+for country in countries:
+
+    country_df = df[df["Area"] == country]
+
+    ts = country_df[["Year","Food Insecurity Rate"]].copy()
+    ts.columns = ["ds","y"]
+    ts["ds"] = pd.to_datetime(ts["ds"], format="%Y")
+
+    model = Prophet()
+    model.fit(ts)
+
+    future = model.make_future_dataframe(periods=5, freq="Y")
+    forecast = model.predict(future)
+
+    joblib.dump(forecast, f"models/forecast/{country}_prophet.pkl")
+
+print("Forecast models saved.")
---
+++ scripts/train_prediction_model.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from utils.preprocess import preprocess_data
+
+df = pd.read_csv("dataset/eda_df.csv")
+
+X, y = preprocess_data(df)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestRegressor(n_estimators=200)
+model.fit(X_train, y_train)
+
+joblib.dump(model, "models/prediction/food_model.pkl")
+
+print("Prediction model saved.")
+
