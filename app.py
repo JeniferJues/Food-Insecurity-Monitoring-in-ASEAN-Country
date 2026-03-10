ae1eb83c9c285ab242678631f16a29ed42716225
Jenifer Mayang Jues <139893426+JMJ-ai@users.noreply.github.com>
2026-03-10 22:56:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ if nav == "Home":
         width:100vw;
         margin-left:calc(-50vw + 50%);
         background-image:
-        linear-gradient(rgba(0,0,0,1), rgba(0,0,0,1)),
+        linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)),
         url('https://static.vecteezy.com/system/resources/thumbnails/013/079/266/small_2x/circle-dot-south-east-asia-map-free-png.png');
         background-size:cover;
         background-position:center;
